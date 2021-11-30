47f7d84c797a725fac098d0e7c1526d8c12c8d1e
fabiarias <47224246+fabiarias@users.noreply.github.com>
2021-11-29 19:39:14 -0500

    Add files via upload

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,18 +1,51 @@
 const express = require("express");
 const cors = require("cors");
+
+//NUEVO PARA EL FUNCIONAMIENTO DEL GRAPHQL
+
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./graphql/resolvers');
+
+
 const app = express();
 
 //SETTINGS
 app.set("port", process.env.PORT || 4000);
 
+
+
 //MIDDLEWARES
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', graphqlHTTP({
+    schema: makeExecutableSchema,
+    rootValue: resolvers,
+    graphiql: true,
+}))
+
+
+
+
 //ROUTES
-app.use("/api/usuarios",require('./Routes/usuariosRoutes'));
+/*app.use("/api/usuarios",require('./Routes/usuariosRoutes'));
 app.use("/api/proyectos", require('./Routes/proyectosRoutes'));
 app.use("/api/inscripciones", require('./Routes/inscripcionesRoutes'));
-app.use("/api/avances", require('./Routes/avancesRoutes'));
+app.use("/api/avances", require('./Routes/avancesRoutes'));*/
+
+
+//CARGAR EL GRAPHQL
+/*app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: makeExecutableSchema,
+      rootValue: resolvers,
+      graphiql: true,
+    })
+  );*/
+
 
 module.exports = app;
